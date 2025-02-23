49bdcbab459bfba4e3fd902b83a0e70ba705c152
visle <almuerto@yandex.ru>
2025-02-24 04:21:37 +0500

    add mock for gophermart services

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,18 +1,1 @@
 package main
-
-import (
-	"fmt"
-
-	"github.com/huandu/go-sqlbuilder"
-)
-
-func main() {
-	sb := sqlbuilder.Update("order")
-	sb.Set(fmt.Sprintf("status = %d", 3))
-	sb.Where(sb.Equal("number", "213123"))
-
-	txt, args := sb.BuildWithFlavor(sqlbuilder.PostgreSQL)
-	_ = args
-
-	fmt.Println(txt)
-}
--- update_mocks.sh
+++ update_mocks.sh
@@ -4,3 +4,8 @@ MOCKBIN=$GOBIN/mockgen
 
 # internal/service/calculation
 $MOCKBIN -package=calculation -destination=internal/service/calculation/repository_mock_test.go -source=internal/repository/calculation/repository.go
+# internal/service/gophermart/
+$MOCKBIN -package=accrual -destination=internal/service/gophermart/accrual/repository_mock_test.go -source=internal/repository/gophermart/repository.go
+$MOCKBIN -package=authorization -destination=internal/service/gophermart/authorization/repository_mock_test.go -source=internal/repository/gophermart/repository.go
+$MOCKBIN -package=order -destination=internal/service/gophermart/order/repository_mock_test.go -source=internal/repository/gophermart/repository.go
+$MOCKBIN -package=withdrawal -destination=internal/service/gophermart/withdrawal/repository_mock_test.go -source=internal/repository/gophermart/repository.go
