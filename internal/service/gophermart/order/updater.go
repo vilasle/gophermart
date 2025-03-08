215511ddda6465599831536f39781379f2bfb34a
visle <almuerto@yandex.ru>
2025-03-08 12:37:53 +0500

    add updater for adding transaction by order and update order state

---
+++ internal/service/gophermart/order/updater.go
@@ -0,0 +1,115 @@
+package order
+
+import (
+	"context"
+
+	"github.com/vilasle/gophermart/internal/logger"
+	"github.com/vilasle/gophermart/internal/repository/gophermart"
+	"github.com/vilasle/gophermart/internal/service"
+)
+
+type updateRepositoryJob struct {
+	userID      string
+	orderNumber string
+	data        service.AccrualsInfo
+}
+
+type updateWorkerConfig struct {
+	orderRepository       gophermart.OrderRepository
+	transactionRepository gophermart.WithdrawalRepository
+}
+
+type updateWorker struct {
+	orderRepository       gophermart.OrderRepository
+	transactionRepository gophermart.WithdrawalRepository
+	input                 <-chan updateRepositoryJob
+}
+
+// responsibility for updating orders states and commit transactions
+func runUpdateWorkers(ctx context.Context, config updateWorkerConfig, input <-chan updateRepositoryJob, qtyWorkers int) {
+	log := logger.With("component", "updater")
+	log.Debug("starting update workers", "qtyWorkers", qtyWorkers)
+
+	for i := 0; i < qtyWorkers; i++ {
+		worker := updateWorker{
+			orderRepository:       config.orderRepository,
+			transactionRepository: config.transactionRepository,
+			input:                 input,
+		}
+
+		go worker.process(ctx)
+	}
+
+	log.Debug("update workers started")
+}
+
+func (e updateWorker) process(ctx context.Context) {
+	log := logger.With("component", "updater instance")
+
+	for {
+		select {
+		case <-ctx.Done():
+			log.Debug("got stop signal. update worker will stop")
+			return
+		case job := <-e.input:
+			log.Debug("got data for updating accrual on repository", "job", job)
+
+			data, status := job.data, defineStatus(job.data.Status)
+
+			go e.postOrderState(ctx, gophermart.OrderUpdateRequest{
+				UserID:  job.userID,
+				Number:  job.orderNumber,
+				Status:  status,
+				Accrual: data.Accrual,
+			})
+
+			if status == gophermart.StatusProcessed {
+				go e.commitTransaction(ctx, gophermart.WithdrawalRequest{
+					UserID:      job.userID,
+					OrderNumber: job.orderNumber,
+					Sum:         data.Accrual,
+				})
+			}
+		}
+	}
+}
+
+func (e updateWorker) commitTransaction(ctx context.Context, dto gophermart.WithdrawalRequest) {
+	log := logger.With("component", "commitTransaction")
+	select {
+	default:
+	case <-ctx.Done():
+		log.Debug("context was canceled, committing of transaction will stop")
+		return
+	}
+	if err := e.transactionRepository.Income(ctx, dto); err != nil {
+		log.Error("adding income to user balance was failed", "dto", dto, "error", err)
+		return
+	}
+}
+
+func (e updateWorker) postOrderState(ctx context.Context, dto gophermart.OrderUpdateRequest) {
+	log := logger.With("component", "postOrderState")
+	select {
+	default:
+	case <-ctx.Done():
+		log.Debug("context was canceled, updater will stop")
+		return
+	}
+
+	if err := e.orderRepository.Update(ctx, dto); err != nil {
+		log.Error("updating order status was failed", "dto", dto, "error", err)
+	}
+}
+
+ func defineStatus(status string) int {
+	switch status {
+	case StatusProcessing:
+		return gophermart.StatusProcessing
+	case StatusInvalid:
+		return gophermart.StatusInvalid
+	case StatusProcessed:
+		return gophermart.StatusProcessed
+	}
+	return gophermart.StatusNew
+}
